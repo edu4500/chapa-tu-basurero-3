55157c86c93c5075af2b822caade28ea4381c082
eduado sutta <eduardo.sg.992@outlook.com>
2018-06-24 04:36:44 -0500

    adasdsa

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -9,7 +9,7 @@ router.post( '/iot' ,iot.CrearUbicacion );
 
 //iot actualizar dispositivos iot
 router.post( '/iot/:iotid' ,iot.ActualizarUbicacion );
-router.post('/iot2/:iotid/:tipo/:estado/:lon/:lat', iot.ActualizarUbicacion2);
+router.get('/iot2/:iotid/:tipo/:estado/:lon/:lat', iot.ActualizarUbicacion2);
 
 //consultar disostivos iot
 router.get( '/iot',iot.getTodos );
